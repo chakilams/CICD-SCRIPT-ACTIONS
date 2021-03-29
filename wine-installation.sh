df80b29d46f1bd893520d643d389a18d569a3160
Santhosh Kumar Chakilam <santhoshchakkilam@gmail.com>
2021-03-29 11:36:24 +0530

    Update wine-installation.sh

--- wine-installation.sh
+++ wine-installation.sh
@@ -3,4 +3,4 @@ wget -nc https://dl.winehq.org/wine-builds/winehq.key
 apt-key add winehq.key
 apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 apt update
-apt install --install-recommends winehq-stable
+apt install -y --install-recommends winehq-stable
