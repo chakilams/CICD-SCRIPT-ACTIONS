cce207f8b7954c59519cd04fa2121d16af5387bf
ansiblejenkins <75774814+ansiblejenkins@users.noreply.github.com>
2021-03-31 17:54:05 +0530

    Update wine-installation.sh

--- wine-installation.sh
+++ wine-installation.sh
@@ -4,3 +4,17 @@ apt-key add winehq.key
 apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 apt update
 apt install -y --install-recommends winehq-stable
+
+mkdir -p /home/AADDS/svc_hermes_qa/sparkparser
+cp -R /home/sshuser/exe/ /home/AADDS/svc_hermes_qa/sparkparser/
+chown svc_hermes_qa:svc_hermes_qa -R /home/AADDS/svc_hermes_qa/sparkparser/* 
+chmod 755 -R /home/AADDS/svc_hermes_qa/sparkparser/exe/ 
+ll /home/AADDS/svc_hermes_qa/sparkparser/exe/
+
+mkdir /home/.wine
+chown svc_hermes_qa:svc_hermes_qa -R /home/.wine 
+chmod 755 -R /home/.wine 
+
+mkdir /home/.local
+chown svc_hermes_qa:svc_hermes_qa -R /home/.local 
+chmod 755 -R /home/.local
