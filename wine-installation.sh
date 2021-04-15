8f8bfb8e1fa33912eb8fc24fc54a86843155f0e4
Santhosh Kumar Chakilam <santhoshchakkilam@gmail.com>
2021-04-15 18:48:33 +0530

    Update wine-installation.sh

--- wine-installation.sh
+++ wine-installation.sh
@@ -1,20 +1,20 @@
-dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-apt-key add winehq.key
-apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
-apt update
-apt install -y --install-recommends winehq-stable
+#dpkg --add-architecture i386
+#wget -nc https://dl.winehq.org/wine-builds/winehq.key
+#apt-key add winehq.key
+#apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
+#apt update
+#apt install -y --install-recommends winehq-stable
 
-#mkdir -p /home/AADDS/svc_hermes_prod/sparkparser
-#cp -R /home/sshuser/exe/ /home/AADDS/svc_hermes_prod/sparkparser/
-#chown svc_hermes_prod:svc_hermes_prod -R /home/AADDS/svc_hermes_prod/sparkparser/* 
-#chmod 755 -R /home/AADDS/svc_hermes_prod/sparkparser/exe/ 
-#ll /home/AADDS/svc_hermes_prod/sparkparser/exe/
+mkdir -p /home/AADDS/svc_hermes_prod/sparkparser
+cp -R /home/sshuser/exe/ /home/AADDS/svc_hermes_prod/sparkparser/
+chown svc_hermes_prod:svc_hermes_prod -R /home/AADDS/svc_hermes_prod/sparkparser/* 
+chmod 755 -R /home/AADDS/svc_hermes_prod/sparkparser/exe/ 
+ll /home/AADDS/svc_hermes_prod/sparkparser/exe/
 
-#mkdir /home/.wine
-#chown svc_hermes_prod:svc_hermes_prod -R /home/.wine 
-#chmod 755 -R /home/.wine 
+mkdir /home/.wine
+chown svc_hermes_prod:svc_hermes_prod -R /home/.wine 
+chmod 755 -R /home/.wine 
 
-#mkdir /home/.local
-#chown svc_hermes_prod:svc_hermes_prod -R /home/.local 
-#chmod 755 -R /home/.local
+mkdir /home/.local
+chown svc_hermes_prod:svc_hermes_prod -R /home/.local 
+chmod 755 -R /home/.local
